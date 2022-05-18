cc435da5aeccd90227bc3d7a763a2ae9ab95b732
gprossliner <6724584+gprossliner@users.noreply.github.com>
2022-05-18 10:31:15 +0200

    run command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func must(err error) {
@@ -23,4 +24,12 @@ func main() {
 
 func run() {
 	fmt.Printf("Running %v UID=%d PID=%d\n", os.Args[1:], os.Geteuid(), os.Getpid())
+
+	// create a exec Command and setup pipes
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Run Command
+
+To test our `run` function, we can execute some commands, like:
+
+* echo hello-world
+* sh (interactive)
+
+Test isolation in interactive shell:
+
+- hostname
+- ps -aux
+- id
